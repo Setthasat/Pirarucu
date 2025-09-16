a37b9dc4a4e31562b8236694506e3f14ac7ee525
Setthasat Sirikomonsing <setthasat@Setthasats-MacBook-Air.local>
2025-09-16 12:03:25 +0700

    add link button hero setion

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -6,10 +6,7 @@ function Hero() {
   return (
     <section className="relative h-screen flex items-center justify-center overflow-hidden">
       <div className="absolute inset-0 z-0 bg-gradient-to-r from-[#134E4A] via-[#115E59] to-[#0F172A]">
-        <img
-          src={HeroImg.src}
-          className="w-full h-full object-cover"
-        />
+        <img src={HeroImg.src} className="w-full h-full object-cover" />
         <div className="absolute inset-0 bg-black/40"></div>
       </div>
 
@@ -26,10 +23,17 @@ function Hero() {
           </p>
         </div>
         <div className="mt-6 flex justify-center items-center gap-6">
-          <a className="bg-[#5EEAD4] w-[40%] text-white font-bold py-3 px-4 rounded flex justify-center items-center  gap-2">
+          <a
+            href="https://en.wikipedia.org/wiki/Arapaima_gigas"
+            target="_blank"
+            className="bg-[#5EEAD4] w-[40%] text-white font-bold py-3 px-4 rounded flex justify-center items-center cursor-pointer gap-2"
+          >
             <Play /> Watch Documentary
           </a>
-          <a href="#feature" className="w-[30%] text-white border font-bold py-3 px-4 rounded cursor-pointer hover:text-[#5EEAD4] hover:border-[#5EEAD4]">
+          <a
+            href="#feature"
+            className="w-[30%] text-white border font-bold py-3 px-4 rounded cursor-pointer hover:text-[#5EEAD4] hover:border-[#5EEAD4]"
+          >
             Explore Facts
           </a>
         </div>
